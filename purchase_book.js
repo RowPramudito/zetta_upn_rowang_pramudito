e333e5d4a05c8a19dd8eb8c86fdae88277e3e4ef
RowPramudito <danielrowang@gmail.com>
2023-03-16 18:06:59 +0700

    Initialization js file for javascript_day_2 task

--- purchase_book.js
+++ purchase_book.js
@@ -1,18 +1,28 @@
 
 
-function book_purchase(title, price, discount, tax) {
+function book_purchase(title, price, discount, tax, quantity, stock) {
     const amountDiscount = price * discount / 100
     let priceAfterDiscount = price - amountDiscount
 
     const amountTax = price * tax / 100
     let priceAfterTax = priceAfterDiscount + amountTax
 
+    let canIbuyMore = "You can still buy more books."
+    for(let i = 1; i <= quantity; i++) {
+        stock -= 1
+        if(quantity >= stock) {
+            break
+            canIbuyMore = "The book is out of stock!"
+        }
+    }
+
     console.log("Book Purchased       : " + title)
     console.log("Original Price       : " + price)
     console.log("Amount of discount   : " + amountDiscount)
     console.log("Price after discount : " + priceAfterDiscount)
     console.log("Amount of tax        : " + amountTax)
-    console.log("Price after tax      : " + priceAfterTax)
+    console.log("Price after tax      : " + priceAfterTax + "\n")
+    console.log(canIbuyMore)
 }
 
 book_purchase("Norwegian Woods", 100000, 20, 5)
