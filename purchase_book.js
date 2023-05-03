b7506d3e0e11080b772973b0b59a0bdf0c5eeab0
RowPramudito <danielrowang@gmail.com>
2023-05-03 19:45:15 +0700

    Adding new parameter "term" in the function. Adding credit functionality to the book purchasing function.

--- purchase_book.js
+++ purchase_book.js
@@ -1,6 +1,6 @@
 
 
-function book_purchase(title, price, discount, tax, quantity, stock) {
+function book_purchase(title, price, discount, tax, quantity, stock, term) {
     const amountDiscount = price * discount / 100
     let priceAfterDiscount = price - amountDiscount
 
@@ -20,6 +20,11 @@ function book_purchase(title, price, discount, tax, quantity, stock) {
         }
     }
 
+    let creditPayment = new Array()
+    for(let i = 0; i < term; i++) {
+        creditPayment[i] = totalPrice / term
+    }
+
     console.log("Book Purchased       : " + title)
     console.log("Original Price       : " + price)
     console.log("Amount of discount   : " + amountDiscount)
@@ -29,10 +34,15 @@ function book_purchase(title, price, discount, tax, quantity, stock) {
     console.log("Total Payment            : " + totalPrice)
     console.log("Amount of tax            : " + amountTax)
     console.log("Total Payment after tax  : " + priceAfterTax + "\n")
+    console.log("Credit Payment per month : ")
+    for(let i = 0; i < creditPayment.length; i++) {
+        process.stdout.write(creditPayment[i] + " ");
+    }
     
+    console.log("\n")
     console.log(stockMessage + "\n")
 }
 
 
-book_purchase("Norwegian Woods", 100000, 20, 5, 10, 9)
-book_purchase("Lelaki Harimau", 85000, 20, 5, 2, 20)
+book_purchase("Norwegian Woods", 100000, 20, 5, 10, 9, 8)
+book_purchase("Lelaki Harimau", 85000, 20, 5, 2, 20, 10)
