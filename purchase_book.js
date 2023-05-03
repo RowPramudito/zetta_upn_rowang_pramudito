29fc80994b9358b349148ba6ecf9814229e0b863
RowPramudito <danielrowang@gmail.com>
2023-05-03 18:54:31 +0700

    Adding total payment to the function.

--- purchase_book.js
+++ purchase_book.js
@@ -4,8 +4,10 @@ function book_purchase(title, price, discount, tax, quantity, stock) {
     const amountDiscount = price * discount / 100
     let priceAfterDiscount = price - amountDiscount
 
-    const amountTax = price * tax / 100
-    let priceAfterTax = priceAfterDiscount + amountTax
+    let totalPrice = priceAfterDiscount * quantity
+
+    const amountTax = totalPrice * tax / 100
+    let priceAfterTax = totalPrice + amountTax
 
     let canIbuyMore = "You can still buy more books."
     for(let i = 1; i <= quantity; i++) {
@@ -20,14 +22,14 @@ function book_purchase(title, price, discount, tax, quantity, stock) {
     console.log("Original Price       : " + price)
     console.log("Amount of discount   : " + amountDiscount)
     console.log("Price after discount : " + priceAfterDiscount)
-    console.log("Amount of tax        : " + amountTax)
-    console.log("Price after tax      : " + priceAfterTax + "\n")
+    console.log("Quantity             : " + quantity + "\n")
+
+    console.log("Amount of tax            : " + amountTax)
+    console.log("Total Payment            : " + totalPrice)
+    console.log("Total Payment after tax  : " + priceAfterTax + "\n")
+    
     console.log(canIbuyMore)
 }
 
 book_purchase("Norwegian Woods", 100000, 20, 5, 10, 9)
 book_purchase("Lelaki Harimau", 85000, 20, 5, 2, 20)
-
-
-
-// the use of for loop was unnecessary but whatever.
